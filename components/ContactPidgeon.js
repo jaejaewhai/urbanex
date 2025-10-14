36e0a3958dcbd02043cdc4f9751de35934c1f557
Justine Chin <jayjaywhy@MacBookPro.ip>
2025-10-14 14:59:23 +1300

    contact pidgeon script update

--- components/ContactPidgeon.js
+++ components/ContactPidgeon.js
@@ -139,7 +139,7 @@ export default function ContactPidgeon() {
       <div className="pigeon-wrapper">
         <div className="pigeon-normal">
           <Image
-            src="/assets/Urbanex_mail.webp"
+            src="/assets/Urbanex_mail.svg"
             alt="Contact-Pidgeon"
             width={553}
             height={367}
@@ -156,7 +156,7 @@ export default function ContactPidgeon() {
         
         <div className="pigeon-hover">
           <Image
-            src="/assets/Urbanex_mail_expand.webp"
+            src="/assets/Urbanex_mail_expand.svg"
             alt="Contact-Pidgeon-Expanded"
             width={553}
             height={367}
