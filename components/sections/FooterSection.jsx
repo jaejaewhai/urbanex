7302d03a7f8adc2e661474907e97023b59e56952
Justine Chin <jayjaywhy@MacBookPro.ip>
2025-10-14 15:54:40 +1300

    logo whiteupload

--- components/sections/FooterSection.jsx
+++ components/sections/FooterSection.jsx
@@ -92,7 +92,7 @@ export default function FooterSection({ id }) {
           }}
         >
           <Image
-            src="/assets/Urbanex_green.svg"
+            src="/assets/Urbanex_white.svg"
             alt="Urbanex Logo"
             width={300}
             height={300}
